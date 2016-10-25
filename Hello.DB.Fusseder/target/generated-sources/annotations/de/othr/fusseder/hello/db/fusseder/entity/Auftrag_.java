7d898efe1b80cf5e3fe76966469405f7fb9b0b83
FlorianFusseder-Desktop <florianfueder@yahoo.de>
2016-10-25 21:59:28 +0200

    Added a maven project to exercise Datastructures for DBs

---
+++ Hello.DB.Fusseder/target/generated-sources/annotations/de/othr/fusseder/hello/db/fusseder/entity/Auftrag_.java
@@ -0,0 +1,15 @@
+package de.othr.fusseder.hello.db.fusseder.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-25T21:34:54")
+@StaticMetamodel(Auftrag.class)
+public class Auftrag_ { 
+
+    public static volatile SingularAttribute<Auftrag, String> bezeichnung;
+    public static volatile SingularAttribute<Auftrag, Long> auftragsId;
+    public static volatile SingularAttribute<Auftrag, Double> gesamtSumme;
+
+}
