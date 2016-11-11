48e037049d5b1c286882b81ca7d4f4bf9bfe3b07
FlorianFusseder-Laptop <florianfueder@yahoo.de>
2016-11-11 13:54:24 +0100

    fixed imports

--- Hello.DBEngineering.Fusseder/src/main/java/Entity/Professor.java
+++ Hello.DBEngineering.Fusseder/src/main/java/Entity/Professor.java
@@ -11,13 +11,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
